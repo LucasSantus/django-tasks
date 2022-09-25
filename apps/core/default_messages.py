66e5b1527c2b639fd3c1e2c8ea11cb24bcd07bc4
Lucas Santos <leos9877@gmail.com>
2022-09-25 12:53:36 -0300

    :rocket: create new file apps/core/default_messages.py

---
+++ apps/core/default_messages.py
@@ -0,0 +1,7 @@
+DEFAULT_MESSAGES = {
+    "ADD": "Registro realizado com sucesso!",
+    "CHANGED": "Registro alterado com sucesso!",
+    "DESACTIVATE": "Registro desativado com sucesso!",
+    "NOT_USER_ACCESS": "Sem permissão de acesso!",
+    "NOT_FOUND": "Registro não encontrado!",
+}
