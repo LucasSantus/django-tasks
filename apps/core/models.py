94a5d927f02ff801d9e4a8ca1415db95f3158d3b
Lucas Santos <leos9877@gmail.com>
2022-09-25 12:53:36 -0300

    :pushpin: change file apps/core/models.py

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    is_active = models.BooleanField(verbose_name = "Ativo", default = True)
+    create_at = models.DateTimeField(verbose_name = "Data & Hora registrado", auto_now_add = True)
+    update_at = models.DateTimeField(verbose_name = "Data & Hora modificado", blank = True, null = True, editable = True)
+    desactivate_at = models.DateTimeField(verbose_name = "Data & Hora desativado", blank = True, null = True, editable = True)
+
+    class Meta:
+        abstract = True
