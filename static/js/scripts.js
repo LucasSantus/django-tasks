a7ad51e5d49fc45fc3d075c89b2d69e0e62cefb0
Lucas Santos <leos9877@gmail.com>
2022-09-25 18:43:05 -0300

    :pushpin: insert changes in file static/js/scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,27 +1,11 @@
-function loadingInit(){
-    $("body").append(`
-        <div class="body__loading">
-            <div class="sk-cube-grid"> <div class="sk-cube-child sk-cube-grid1"></div> <div class="sk-cube-child sk-cube-grid2"></div> <div class="sk-cube-child sk-cube-grid3"></div> <div class="sk-cube-child sk-cube-grid4"></div> <div class="sk-cube-child sk-cube-grid5"></div> <div class="sk-cube-child sk-cube-grid6"></div> <div class="sk-cube-child sk-cube-grid7"></div> <div class="sk-cube-child sk-cube-grid8"></div> <div class="sk-cube-child sk-cube-grid9"></div> </div>
-        </div>
-    `)
+function build__todos(items, id_class){
+    $("#" + id_class).empty();
+
+    for( let item of items ){
+        $(`
+            <div class="bg-danger w-100">
+                ${item.title}
+            </div>
+        `).appendTo("#" + id_class);
+    }
 }
-
-function loadingStop(){
-    $(".body__loading").remove();
-}
-
-$(document).ready(function(){
-    $(window).bind('beforeunload', function() {
-        console.log("abriu")
-        loadingInit();
-    });
-
-    $(window).on("load", async function() {
-        await new Promise(function(resolve) {
-            
-            loadingStop();
-            resolve();
-        });
-        console.log("fechou")
-    }) 
-});
