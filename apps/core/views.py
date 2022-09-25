3fa02bfc51814dc5aed7d88babb980a92b20b708
Lucas Santos <leos9877@gmail.com>
2022-09-25 12:53:36 -0300

    :pushpin: change file apps/core/views.py

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from todo.models import Todo
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def error_404(request, exception):
+    return render(request, '404.html', status = 404)
+
+@login_required
+def index(request):
+    todos = Todo.objects.prefetch_related("user").filter(user = request.user)
+
+    context = {
+        "todos": todos,
+    }
+
+    return render(request, 'core/index.html', context)
