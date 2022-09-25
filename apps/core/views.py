6d0e1d6bbb9e43b219d9eb37ae22f5a82e37eee1
Lucas Santos <leos9877@gmail.com>
2022-09-25 18:43:04 -0300

    :pushpin: insert changes in view index for adapt jquery on template apps/core/views.py

--- apps/core/views.py
+++ apps/core/views.py
@@ -4,16 +4,21 @@ from django.contrib.auth.decorators import login_required
 from todo.models import Todo
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
+from core.models import convert_object_in_json
+from todo.forms import TodoForm
 
 def error_404(request, exception):
     return render(request, '404.html', status = 404)
 
 @login_required
 def index(request):
-    todos = Todo.objects.prefetch_related("user").filter(user = request.user)
+    todos = Todo.objects.prefetch_related("user").filter(user = request.user).values()
+
+    form = TodoForm()
 
     context = {
-        "todos": todos,
+        "todos": convert_object_in_json(todos),
+        'form': form
     }
 
     return render(request, 'core/index.html', context)
