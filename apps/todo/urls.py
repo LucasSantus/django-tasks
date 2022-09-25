be42120b95719ff6b218adf476b9bab640269e0e
Lucas Santos <leos9877@gmail.com>
2022-09-25 18:43:04 -0300

    :pushpin: comment urls apps/todo/urls.py

--- apps/todo/urls.py
+++ apps/todo/urls.py
@@ -4,9 +4,9 @@ from .views import *
 urlpatterns = [
     path('todos/', include([
         path('create/', create_todo, name='create_todo'),
-        path('<slug:slug_todo>', include([
-            path('change/', change_todo, name="change_todo"),
-            path("delete/", delete_todo, name="delete_todo"),
-        ]))
+        # path('<slug:slug_todo>', include([
+        #     path('change/', change_todo, name="change_todo"),
+        #     path("delete/", delete_todo, name="delete_todo"),
+        # ]))
     ])),
 ]
