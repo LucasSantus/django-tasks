0f41b689089af542799f554b279b85602564f39e
Lucas Santos <leos9877@gmail.com>
2022-09-26 00:09:18 -0300

    :pushpin: change view for create new todo in file apps/todo/views.py

--- apps/todo/views.py
+++ apps/todo/views.py
@@ -7,24 +7,16 @@ from django.core import serializers
 
 # CATEGORY
 def create_todo(request):
-    # request should be ajax and method should be POST.
     if request.is_ajax and request.method == "POST":
-        # get the form data
         form = TodoForm(request.POST)
-        # save the data and after fetch the object in instance
         if form.is_valid():
-            instance = form.save(commit = False)
-            instance.user = request.user
-            instance.save()
-
-            # serialize in new friend object in json
-            ser_instance = serializers.serialize('json', [ instance, ])
-
-            # send to client side.
-            return JsonResponse({"instance": ser_instance}, status = 200)
+            data = form.save(commit = False)
+            data.user = request.user
+            data.save()
+            
+            data_serialized = serializers.serialize('json', [ data ])
+            return JsonResponse({"data": data_serialized}, status = 200)
         else:
-            # some form errors occured.
             return JsonResponse({"error": form.errors}, status = 400)
 
-    # some error occured
     return JsonResponse({"error": ""}, status=400)
