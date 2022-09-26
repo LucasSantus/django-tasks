ac6c8f0d81d0d8a3993c23307d14c2b83e64a42a
Lucas Santos <leos9877@gmail.com>
2022-09-26 00:09:18 -0300

    :pushpin: adapt function for list todos in file static/js/scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,11 +1,19 @@
-function build__todos(items, id_class){
-    $("#" + id_class).empty();
+const build_html = ( item ) => {
+    return `
+        <div class="bg-danger w-100">
+            ${item.title}
+        </div>
+    `
+}
 
-    for( let item of items ){
-        $(`
-            <div class="bg-danger w-100">
-                ${item.title}
-            </div>
-        `).appendTo("#" + id_class);
+const build_todo = ( values, id_class ) => $( build_html(values) ).appendTo("#" + id_class);
+
+function get_all_todos(values, id_class){
+    if( values.length > 1 ) {
+        for( let item of values ){
+            build_todo(item, id_class);
+        }
+    } else {
+        build_todo(values, id_class);
     }
 }
