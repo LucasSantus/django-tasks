247badd02cba65c94d34ae5efac721a89cb39265
Lucas Santos <leos9877@gmail.com>
2022-09-27 00:55:34 -0300

    :pushpin: change const build_html

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,14 +1,8 @@
-const build_html = ( item ) => {
-    return `
-        <div class="bg-danger w-100">
-            ${item.title}
-        </div>
-    `
-}
+const build_html = ( item ) => `<div class="bg-danger w-100"> ${item.title} </div>`;
 
 const build_todo = ( values, id_class ) => $( build_html(values) ).appendTo("#" + id_class);
 
-function get_all_todos(values, id_class){
+const get_all_todos = (values, id_class) => {
     if( values.length > 1 ) {
         for( let item of values ){
             build_todo(item, id_class);
