9bf3f6b9800f1237bee87cd4f5a3834e84aed929
Lucas Santos <leos9877@gmail.com>
2022-09-28 00:39:58 -0300

    :rocket: add new file static/js/functions/todos.js

---
+++ static/js/functions/todos.js
@@ -0,0 +1,22 @@
+const build_html = ( item ) => `<div class="bg-danger w-100"> ${ item.title } </div>`;
+
+const build_todo = ( item, id ) => $( build_html(item) ).appendTo("#" + id);
+
+const remove_class = ( element, items ) => {
+    for( let item of items ) $(element).removeClass(item);
+}
+
+const get_all_todos = ( method, response, id ) => {
+    let values = method === "GET" ? 
+        JSON.parse(response) : JSON.parse(response["data"])[0]["fields"];
+
+    if( values.length > 1 ){
+        for( let item of values ) build_todo(item, id);
+    } else {
+        build_todo(values, id);
+    }
+
+    $('.modal').modal('hide');
+
+    remove_class( ".form-control", [ 'is-valid', 'is-invalid' ] );
+}
