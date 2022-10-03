2e4188f267891c89dcc9ab5e9ce032b76b6c50fb
Lucas Santus <leos9877@gmail.com>
2022-10-02 22:00:38 -0300

    :pushpin: change code in file apps/tasks/urls.py

--- apps/tasks/urls.py
+++ apps/tasks/urls.py
@@ -4,9 +4,9 @@ from .views import *
 urlpatterns = [
     path('tasks/', include([
         path('create/', create_task, name='create_task'),
-        # path('<slug:slug_task>', include([
-        #     path('change/', change_task, name="change_task"),
-        #     path("delete/", delete_task, name="delete_task"),
-        # ]))
+        path('<slug:slug_task>', include([
+            # path('change/', change_task, name="change_task"),
+            path("delete/", remove_task, name="remove_task"),
+        ]))
     ])),
 ]
