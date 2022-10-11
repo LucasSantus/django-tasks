a27e948a3a5acea61d839c0bf9104bb7a771dfc0
Lucas Santos <leos9877@gmail.com>
2022-10-11 00:45:49 -0300

    :pushpin: insert changes in code for file apps/core/models.py

--- apps/core/models.py
+++ apps/core/models.py
@@ -9,7 +9,7 @@ class BaseModel(models.Model):
     is_active = models.BooleanField(verbose_name = "Ativo", default = True)
     create_at = models.DateTimeField(verbose_name = "Data & Hora registrado", auto_now_add = True)
     update_at = models.DateTimeField(verbose_name = "Data & Hora modificado", blank = True, null = True, editable = True)
-    desactivate_at = models.DateTimeField(verbose_name = "Data & Hora desativado", blank = True, null = True, editable = True)
+    deactivate_at = models.DateTimeField(verbose_name = "Data & Hora desativado", blank = True, null = True, editable = True)
 
     class Meta:
         abstract = True
